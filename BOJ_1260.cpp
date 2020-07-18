0c5ab1a7cf79bcad7906b3ebb104cbf6e518d3a0
yeoneeeee <64140940+yeoneeeee@users.noreply.github.com>
2020-07-19 00:27:32 +0900

    Update BOJ_1260.cpp
    2020.7.18 - BOJ_1260

--- BOJ_1260.cpp
+++ BOJ_1260.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int N, M, V;
 int visited[1001] = { 0, }; //방문표시
-int x[1001][1001]; //인접행렬
+int x[1001][1001]; //그래프 탐색 - 인접행렬 이용
 
 void DFS(int V) {
 	cout << V << " ";
@@ -48,7 +48,9 @@ int main() {
 
 	DFS(V);
 	cout << endl;
-	memset(visited, 0, sizeof(visited)); //다시 초기화
+
+	//배열 초기화 - memset(시작포인터, 초기화내용, 사이즈), <cstring>헤더
+	memset(visited, 0, sizeof(visited));
 
 	BFS(V);
 	cout << endl;
